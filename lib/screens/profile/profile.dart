f976f57c3c3154627519cda2b69ac495deaf7397
Leonardo Cesías Díaz <lpcd_04@hotmail.com>
2024-09-24 12:34:06 -0500

    Added profile screen

---
+++ lib/screens/profile/profile.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundColor: Colors.grey.shade300,
+            ),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(labelText: 'Name', border: OutlineInputBorder()),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(labelText: 'ID', border: OutlineInputBorder()),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(labelText: 'Email', border: OutlineInputBorder()),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(labelText: 'Address', border: OutlineInputBorder()),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              width: double.infinity,
+              child: OutlinedButton(
+                onPressed: () {
+                  // Implement log out functionality
+                },
+                style: OutlinedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(vertical: 20),
+                  backgroundColor: Colors.orange,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  side: BorderSide(
+                    color: Colors.orange,
+                    width: 2,
+                  ),
+                ),
+                child: Text(
+                  'REGISTER',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
