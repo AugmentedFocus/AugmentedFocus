02ee6cc9dbc5c40cc08fe6c85af822d0fb99ccfe
Leonardo Cesías Díaz <lpcd_04@hotmail.com>
2024-09-24 12:33:59 -0500

    Added upcoming events screen

---
+++ lib/screens/courses/upcoming_events.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class UpcomingEventsScreen extends StatelessWidget {
+  Future<List<String>> fetchEvents() async {
+    var response = await http.get(Uri.parse("https://corporatebs-generator.sameerkumar.website"));
+    var jsonResponse = json.decode(response.body);
+    return List.generate(5, (index) => jsonResponse['phrase']);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Upcoming Events'),
+      ),
+      body: FutureBuilder(
+        future: fetchEvents(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error loading data'));
+          } else {
+            List<String> events = snapshot.data as List<String>;
+            return ListView.builder(
+              itemCount: events.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  color: Colors.orangeAccent,
+                  child: ListTile(
+                    title: Text(events[index]),
+                    subtitle: Text('Due Date: Some date'),
+                    trailing: Text('Time left'),
+                  ),
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
