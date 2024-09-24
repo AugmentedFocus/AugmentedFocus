b7d0f7c482163bab0d2b21ac2a50b2babc141284
Leonardo Cesías Díaz <lpcd_04@hotmail.com>
2024-09-24 13:04:01 -0500

    Added navbar

--- lib/shared/navbar.dart
+++ lib/shared/navbar.dart
@@ -1,25 +1,66 @@
 import 'package:flutter/material.dart';
 
 class NavBar extends StatelessWidget {
-  final int currentIndex;
-  final Function(int) onTap;
-
-  const NavBar({Key? key, required this.currentIndex, required this.onTap}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      currentIndex: currentIndex,
-      onTap: onTap,
-      backgroundColor: Colors.black, // Fondo negro
-      selectedItemColor: Colors.white, // Color del ítem seleccionado
-      unselectedItemColor: Colors.grey, // Color de los ítems no seleccionados
-      items: [
-        BottomNavigationBarItem(icon: Icon(Icons.calendar_today), label: 'Events'),
-        BottomNavigationBarItem(icon: Icon(Icons.book), label: 'Courses'),
-        BottomNavigationBarItem(icon: Icon(Icons.grade), label: 'Grades'),
-        BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today),
+          label: 'Events',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.book),
+          label: 'Courses',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.grade),
+          label: 'Grades',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
       ],
+      currentIndex: _getSelectedIndex(context),
+      selectedItemColor: Colors.orange,
+      unselectedItemColor: Colors.black,
+      onTap: (index) {
+        _onItemTapped(index, context);
+      },
     );
+  }
+
+  int _getSelectedIndex(BuildContext context) {
+    final route = ModalRoute.of(context)?.settings.name;
+    switch (route) {
+      case '/upcoming-events':
+        return 0;
+      case '/courses':
+        return 1;
+      case '/grades':
+        return 2;
+      case '/profile':
+        return 3;
+      default:
+        return 1;
+    }
+  }
+
+  void _onItemTapped(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        Navigator.pushReplacementNamed(context, '/upcoming-events');
+        break;
+      case 1:
+        Navigator.pushReplacementNamed(context, '/courses');
+        break;
+      case 2:
+        Navigator.pushReplacementNamed(context, '/grades');
+        break;
+      case 3:
+        Navigator.pushReplacementNamed(context, '/profile');
+        break;
+    }
   }
 }
