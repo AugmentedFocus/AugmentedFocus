741d8a944ac61af2380543541ebd3f24c418877d
Leonardo Cesías Díaz <lpcd_04@hotmail.com>
2024-10-31 15:01:57 -0500

    Fixed main

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,21 @@
-import 'package:augmentedfocus/screens/courses/courses.dart';
-import 'package:augmentedfocus/screens/courses/grades.dart';
-import 'package:augmentedfocus/screens/courses/upcoming_events.dart';
 import 'package:augmentedfocus/screens/login/login_screen.dart';
-import 'package:augmentedfocus/screens/login/register_screen.dart';
 import 'package:augmentedfocus/screens/login/welcome_screen.dart';
-import 'package:augmentedfocus/screens/profile/profile.dart';
+import 'package:augmentedfocus/shared/navbar_roots.dart';
 import 'package:flutter/material.dart';
 
-import 'config/theme/app_theme.dart';
-
-void main() async {
-  runApp(MyApp());
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false, // Optional: Remove the debug banner
-      title: 'Augmented Focus App', // Optional: Set a title for your app
-      theme: AppTheme.theme, // Assuming you have a defined theme
-      initialRoute: '/', // Set initial route to welcome screen
-      routes: {
-        '/': (context) => const WelcomeScreen(),
-        '/welcome': (context) => WelcomeScreen(),
-        '/register': (context) => RegisterScreen(),
-        '/login': (context) => LoginScreen(),
-        '/courses': (context) => CoursesScreen(),
-        '/grades': (context) => GradesScreen(),
-        '/upcoming-events': (context) => UpcomingEventsScreen(),
-        '/profile': (context) => ProfileScreen(),
-      },
+      debugShowCheckedModeBanner: false,
+      title: 'AugmentedFocus',
+      home: WelcomeScreen(),
     );
   }
 }
