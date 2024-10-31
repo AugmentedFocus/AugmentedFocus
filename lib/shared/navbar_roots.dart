8fd96771d615e58dd28cbdb1b74acc99f1250ee7
Leonardo Cesías Díaz <lpcd_04@hotmail.com>
2024-10-31 15:02:05 -0500

    Improved navbar

---
+++ lib/shared/navbar_roots.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import '../screens/courses/courses.dart';
+import '../screens/courses/grades.dart';
+import '../screens/courses/upcoming_events.dart';
+import '../screens/profile/profile.dart';
+
+class NavBarRoots extends StatefulWidget {
+  const NavBarRoots({super.key});
+
+  @override
+  State<NavBarRoots> createState() => _NavBarRootsState();
+}
+
+class _NavBarRootsState extends State<NavBarRoots> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = [
+    const UpcomingEventsScreen(),
+    const CoursesScreen(),
+    const GradesScreen(),
+    const ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.orange,
+        unselectedItemColor: Colors.black,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Events',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book),
+            label: 'Courses',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.grade),
+            label: 'Grades',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
